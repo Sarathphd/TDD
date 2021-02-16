b333edf1cfde913813c3622222fe996cfeea5bf5
sarathkumarmanjunathan <sarathkumarmanjunathan@sarathkumarsair>
2021-02-16 22:23:52 +0530

    initial project

---
+++ mytdd/src/main/java/com/sarath/MytddApplication.java
@@ -0,0 +1,13 @@
+package com.sarath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytddApplication.class, args);
+	}
+
+}
---
+++ mytdd/src/main/java/com/sarath/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sarath;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MytddApplication.class);
+	}
+
+}
